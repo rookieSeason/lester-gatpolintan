0883606aee44fcd98eaa96e9975a6f244cdf2f56
Lester <gatpolintanjohn@gmail.com>
2024-02-29 17:18:33 +0800

    files added

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <footer className="bg-white text-gray-700 dark:bg-[#243447] dark:text-gray-300 duration-300">
-            <div className="w-screen mx-auto max-w-screen-xl p-4 md:flex md:items-center justify-center">
-                <span className="text-xs sm:text-md">© 2023 <a href="/" className="hover:underline">Lester John Gatpolintan™</a>. All Rights Reserved.
-                </span>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-white text-gray-700 dark:bg-[#243447] dark:text-gray-300 duration-300">
+      <div className="w-screen mx-auto max-w-screen-xl p-4 md:flex md:items-center justify-center">
+        <span className="text-sm sm:text-lg">
+          © 2023{" "}
+          <a href="/" className="hover:underline">
+            Lester John Gatpolintan™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
