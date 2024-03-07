081a232eb0e79731ee435326bf49081934eff14f
Lester <gatpolintanjohn@gmail.com>
2024-03-07 09:34:23 +0800

    Footer data become dynamic

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="bg-white text-gray-700 dark:bg-[#243447] dark:text-gray-300 duration-300">
       <div className="w-screen mx-auto max-w-screen-xl p-4 md:flex md:items-center justify-center">
         <span className="text-sm sm:text-lg">
-          © 2023{" "}
+          © {new Date().getFullYear()}{" "}
           <a href="/" className="hover:underline">
             Lester John Gatpolintan™
           </a>
