b1360fdb8cf72565a527a754b2bec1d22102bab6
Lester <gatpolintanjohn@gmail.com>
2024-06-11 16:46:06 +0800

    night mode button adjusted

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,7 +22,7 @@ function Sidebar() {
     <div>
       <button
         onClick={() => toggleDarkMode()}
-        class="z-20 fixed bottom-20 right-8 text-2xl text-center border border-gray-300 bg-white text-gray-600 p-4 shadow-md rounded-full hover:bg-gray-300 dark:bg-[#424242] dark:hover:bg-gray-700 dark:text-gray-300"
+        class="z-20 fixed bottom-20 right-4 text-2xl text-center border border-gray-300 bg-white text-gray-600 p-4 shadow-md rounded-full hover:bg-gray-300 dark:bg-[#424242] dark:hover:bg-gray-700 dark:text-gray-300"
       >
         {isDarkMode ? <MdSunny /> : <FaMoon />}
       </button>
